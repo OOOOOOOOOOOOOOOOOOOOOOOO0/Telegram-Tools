f9b5145f89206944cddbe03d94b57bc52c3758b7
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-21 22:28:59 +0000

    Create swapper.py

---
+++ Swapper/TelegramSwapper/swapper.py
@@ -0,0 +1,39 @@
+import os, tgcrypto
+from telethon.sync import TelegramClient
+from telethon import functions, types
+from pyrogram import Client
+from tprint import *
+
+# swapper.py
+# Author: execution
+# Date: 21/02/2022
+
+class TelegramSwapper:
+
+    def __init__(self):
+        self.tries = 0
+        self.available = False
+    
+    def Clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def Swapper(self):
+        while True:
+            result = self.client(functions.account.CheckUsernameRequest(username=self._target))
+            self.tries += 1
+
+            if result == True:
+                self.available = True
+            else:
+                pass
+            if self.available == True:
+                self.client(functions.channels.UpdateUsernameRequest(channel=self._current, username=self._target))
+                tprint(tprint.GREEN, f"Successfully claimed @{self._target}", 13)
+    
+    def Swap(self, api_id, api_hash, current, target):
+        self._current = current
+        self._target = target
+        self.client = TelegramClient('anon', api_id, api_hash)
+        self.client.start()
+        self.Clear()
+        self.Swapper()
