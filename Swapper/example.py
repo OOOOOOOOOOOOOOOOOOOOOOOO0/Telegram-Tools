a05160495d30b61443eb0a106b92084f668e2ff0
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-21 22:40:11 +0000

    Delete example.py

--- Swapper/example.py
+++
@@ -1,9 +0,0 @@
-from TelegramSwapper import TelegramSwapper
-
-api_id = input("Enter API_ID: ")
-api_hash = input("Enter API_HASH: ")
-current = input("Current channel name: ")
-target = input("Target channel name: ")
-
-t = TelegramSwapper()
-t.Swap(api_id, api_hash, current, target)
