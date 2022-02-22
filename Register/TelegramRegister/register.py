a3f94998a93e982fea9d75bb115c1072c0d6b03d
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-22 11:39:14 +0000

    Create register.py

---
+++ Register/TelegramRegister/register.py
@@ -0,0 +1,40 @@
+from telethon.sync import TelegramClient
+from telethon import functions, types
+from pyrogram import Client
+from tprint import *
+import time
+
+# register.py
+# Author: execution
+# Date: 22/2/22 (nice)
+
+class TelegramRegister:
+
+    def __init__(self):
+        pass
+
+
+    def Register(self, target, api_id, api_hash):
+        client_t = TelegramClient('Checker', api_id, api_hash)
+        client_t.start()
+        client = Client("spad_sec", api_id, api_hash)
+        client.start()
+
+        
+        available = False
+
+        while available == False:
+            result = client_t(functions.account.CheckUsernameRequest(username=target))
+            time.sleep(2)
+            if result == True:
+                try:
+                    channel = client.create_channel(f"@{target}", "This channel was created by")#https://github.com/execution/Telegram-Tools")
+                    client.update_chat_username(channel.id, target)
+                    print(f"Claimed @{target}")
+                except Exception as err:
+                    print(err)
+            else:
+                quit()
+
+
+        
